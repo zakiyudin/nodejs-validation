823d3952ff6b23e823b1c538fbb7d01aedadb88f
ezkam <kf.zakiyudin@gmal.com>
2023-06-19 14:34:26 +0700

    validation nested object

--- tests/validate-object.test.js
+++ tests/validate-object.test.js
@@ -4,12 +4,20 @@ describe('Joi', () => {
   it('should be can validate object', () => {
     const loginSchema = Joi.object({
         username: Joi.string().email().min(5).max(100).required(),
-        password: Joi.string().min(3).max(10).required()
+        password: Joi.string().min(3).max(10).required(),
+        address: Joi.object({
+            street: Joi.string().required(),
+            postcode: Joi.string().required()
+        }).required()
     })
 
     const request = {
         username: "zaki@mail.com",
-        password: "rahasia"
+        password: "rahasia",
+        address: {
+            street: "jl.ahmad yani",
+            postcode: "61382"
+        }
     }
 
     const result = loginSchema.validate(request, {
